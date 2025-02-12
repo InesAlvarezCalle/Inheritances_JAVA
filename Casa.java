048b4d0362b2f70532e515475fc44257526038b0
InesAlvarezCalle <inesalvarezdaw@gmail.com>
2025-02-12 13:29:28 +0100

    Añadimos la clase Piso que viene de Casa

--- Casa.java
+++ Casa.java
@@ -95,3 +95,29 @@ class Adosado extends Casa{
         return patio;
     }
 }
+
+class Piso extends Casa{
+    private int nplanta, nbalcones;
+
+    public Piso(int puertas, int ventanas, String color, int np, int nb){
+        super(puertas, ventanas, color);
+        this.nplanta = np;
+        this.nbalcones = nb;
+    }
+
+    public void setNplanta(int x){
+        this.nplanta = x;
+    }
+
+    public int getNplanta(){
+        return nplanta;
+    }
+
+    public void setNbalcones(int x){
+        this.nbalcones = x;
+    }
+
+    public int getNbalcones(){
+        return nbalcones;
+    }
+}
